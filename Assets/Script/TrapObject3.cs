3f513159db1a19642c258755f33974bbe96ee3ea
myth0629 <bruno0629@naver.com>
2024-11-16 17:49:36 +0900

    update(11.16)
    함정 추가, 타일맵 수정 및 업데이트

---
+++ Assets/Script/TrapObject3.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class DamageObject3 : MonoBehaviour
+{
+    public float speed = 5f;  // 오브젝트의 이동 속도
+    public float lifetime = 3f;  // 오브젝트가 존재하는 시간 (초)
+    public float damageAmount = 10f;  // 데미지 양
+
+    private float distanceTraveled = 0f;
+
+    void Start()
+    {
+        // lifetime 후에 오브젝트를 파괴
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 플레이어에게 데미지를 입힘
+            HeroKnight playerHealth = other.GetComponent<HeroKnight>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount);
+                Debug.Log("Player Hit!!");
+            }
+        }
+        Destroy(gameObject);
+    }
+}
